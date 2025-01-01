14bd08df5f2b37e9808eac7af3f39e27bb845cef
Pavneesh52 <pavneeshkumar1506@gmail.com>
2025-01-01 15:40:56 +0530

    Side Paddles

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
+
 
 screen  = Screen()
 screen.bgcolor("black")
@@ -9,6 +13,8 @@ screen.tracer(0)
 
 r_paddle = Paddle((375, 0))
 l_paddle = Paddle((-380, 0))
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -18,6 +24,25 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1 )
     screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340:
+        ball.bounce_x()
+
+    if ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
+
+    if ball.xcor() > 375:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
